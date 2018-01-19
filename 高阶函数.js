a5bd5c562c4cce8ca1fc3a8294a62437d72ce9de
chenfengami <541307194@qq.com>
2018-01-19 11:56:59 +0800

    single model

--- 高阶函数.js
+++ 高阶函数.js
@@ -1,9 +1,10 @@
 /*
-    高级函数特点
+    高阶函数特点
     1.使用函数作为参数
     2.使用函数作为返回值
 */
 
+// 函数作为参数
 // 在页面创建100个div节点，并且将这些节点都隐藏
 
 // 第一种编码方式
@@ -35,3 +36,63 @@ var appendDiv = function(callback){
 appendDiv(function(node){
     node.style.display = 'none';
 })
+
+
+//函数作为返回值
+
+var isString = function(obj){
+    return Object.prototype.toString.call(obj) === '[object String]';
+}
+
+var isArray = function(obj){
+    return Object.prototype.toString.call(obj) === '[object Array]';    
+}
+
+var isNumber = function(obj){
+    return Object.prototype.toString.call(obj) === '[object Number]';
+}
+
+//以上代码大部分实现都是相同的，唯一不同的是返回的字符串 因此提前把字符串传入进去
+
+var isType = function(type){
+    return function(obj){
+        return Object.prototype.toString.call(obj) === '[object '+ type +']';
+    }
+}
+
+var isString = isType('String');
+var isArray = isType('Array');
+var isNumber = isType('Number');
+
+console.log(isArray([1, 2, 3])); //输出： true
+
+//我们还可以用for循环批量注册这些isType函数 利用之前的闭包存储局部变量 防止for循环异步刷新导致变量存储失效
+
+var Type = {};
+
+for(var i = 0, type; type = ['String', 'Array', 'Number'][i++];){
+    (function(type){
+        Type['is' + type] = function(obj){
+            return Object.prototype.toString.call(obj) === '[object '+ type +']';
+        }
+    })(type)
+};
+
+Type.isArray([1, 2, 3]); //输出： true
+
+
+//单例类型 getSingle让函数成为参数 又在返回的时候返回一个函数
+var getSingle = function(fn){
+    var ret;
+    return function(){
+        return ret || (ret = fn.apply(this, arguments))
+    };
+}
+
+
+var getScript = getSingle(function(){
+    return document.createElement('script');
+});
+var script1 = getScript();
+var script2 = getScript();
+console.log(script1 === script2);
